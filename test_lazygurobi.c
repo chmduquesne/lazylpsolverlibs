a56c2112c0552c15e6b436e1a382cea6bcbc796b
Christophe-Marie Duquesne <chm.duquesne@gmail.com>
2011-02-17 18:26:13 +0100

    Wrote real tests (taken from the respective docs of each solver)

--- test_lazygurobi.c
+++ test_lazygurobi.c
@@ -1,7 +1,132 @@
 #include "lazy_gurobi_c.h"
+#include "test_lazylibs.h"
 
-int main(int argc, const char *argv[])
+
+/* Copyright 2010, Gurobi Optimization, Inc. */
+
+/* This example formulates and solves the following simple MIP model:
+
+     minimize   -x -   y - 2 z
+     subject to  x + 2 y + 3 z <= 4
+                 x +   y       >= 1
+     x, y, z binary
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int
+solve()
 {
+  GRBenv   *env   = NULL;
+  GRBmodel *model = NULL;
+  int       error = 0;
+  double    sol[3];
+  int       ind[3];
+  double    val[3];
+  double    obj[3];
+  char      vtype[3];
+  int       optimstatus;
+  double    objval;
+  int       zero = 0;
 
-    return 0;
+  /* Create environment */
+
+  error = GRBloadenv(&env, "mip1.log");
+  if (error || env == NULL) {
+    fprintf(stderr, "Error: could not create environment\n");
+    exit(1);
+  }
+
+  /* Create an empty model */
+
+  error = GRBnewmodel(env, &model, "mip1", 0, NULL, NULL, NULL, NULL, NULL);
+  if (error) goto QUIT;
+
+
+  /* Add variables */
+
+  obj[0] = -1; obj[1] = -1; obj[2] = -2;
+  vtype[0] = GRB_BINARY; vtype[1] = GRB_BINARY; vtype[2] = GRB_BINARY;
+  error = GRBaddvars(model, 3, 0, NULL, NULL, NULL, obj, NULL, NULL, vtype,
+                     NULL);
+  if (error) goto QUIT;
+
+  /* Integrate new variables */
+
+  error = GRBupdatemodel(model);
+  if (error) goto QUIT;
+
+
+  /* First constraint: x + 2 y + 3 z <= 4 */
+
+  ind[0] = 0; ind[1] = 1; ind[2] = 2;
+  val[0] = 1; val[1] = 2; val[2] = 3;
+
+  error = GRBaddconstr(model, 3, ind, val, GRB_LESS_EQUAL, 4.0, "c0");
+  if (error) goto QUIT;
+
+  /* Second constraint: x + y >= 1 */
+
+  ind[0] = 0; ind[1] = 1;
+  val[0] = 1; val[1] = 1;
+
+  error = GRBaddconstr(model, 2, ind, val, GRB_GREATER_EQUAL, 1.0, "c1");
+  if (error) goto QUIT;
+
+  /* Optimize model */
+
+  error = GRBoptimize(model);
+  if (error) goto QUIT;
+
+  /* Write model to 'mip1.lp' */
+
+  error = GRBwrite(model, "mip1.lp");
+  if (error) goto QUIT;
+
+  /* Capture solution information */
+
+  error = GRBgetintattr(model, GRB_INT_ATTR_STATUS, &optimstatus);
+  if (error) goto QUIT;
+
+  error = GRBgetdblattr(model, GRB_DBL_ATTR_OBJVAL, &objval);
+  if (error) goto QUIT;
+
+  error = GRBgetdblattrarray(model, GRB_DBL_ATTR_X, 0, 3, sol);
+  if (error) goto QUIT;
+
+  printf("\nOptimization complete\n");
+  if (optimstatus == GRB_OPTIMAL) {
+    printf("Optimal objective: %.4e\n", objval);
+
+    printf("  x=%.0f, y=%.0f, z=%.0f\n", sol[0], sol[1], sol[2]);
+  } else if (optimstatus == GRB_INF_OR_UNBD) {
+    printf("Model is infeasible or unbounded\n");
+  } else {
+    printf("Optimization was stopped early\n");
+  }
+
+QUIT:
+
+  /* Error reporting */
+
+  if (error) {
+    printf("ERROR: %s\n", GRBgeterrormsg(env));
+    exit(1);
+  }
+
+  /* Free model */
+
+  GRBfreemodel(model);
+
+  /* Free environment */
+
+  GRBfreeenv(env);
+
+  return 0;
+}
+
+
+int main(int argc, const char *argv[]) {
+    return test_lazysolver(&load_gurobi_c_symbols, &solve, &unload_gurobi_c_symbols);
 }
