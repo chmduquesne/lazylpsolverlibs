bcc6261075371b8f62e62479338ebb533ceb043f
Christophe-Marie Duquesne <chm.duquesne@gmail.com>
2011-02-17 15:57:34 +0100

    Added test programs (not doing anything right now) that will be shipped with the lib: They will enable the users to test the lib is working: try to detect the solver and run a little problem if present.

---
+++ test_lazyglpk.c
@@ -0,0 +1,7 @@
+#include "lazy_glpk.h"
+
+int main(int argc, const char *argv[])
+{
+
+    return 0;
+}
---
+++ test_lazyxprs.c
@@ -0,0 +1,8 @@
+#include "lazy_xprs.h"
+
+int main(int argc, const char *argv[])
+{
+    int status;
+    status = load_xprs_symbols();
+    return 0;
+}
