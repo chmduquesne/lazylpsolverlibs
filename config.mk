2da92062b4c68c8209edfa90932c2ae13a153260
Christophe-Marie Duquesne <chm.duquesne@gmail.com>
2012-02-23 23:21:17 +0100

    Compiling fine

--- config.mk
+++ config.mk
@@ -7,10 +7,10 @@ PREFIX=/usr/local
 # flags
 #CFLAGS=-ansi -Wall -Os -I tools/include
 #CFLAGS=-ansi -Wall -Os -I tools/include -I ~/.wine/drive_c/Program\ Files\ \(x86\)/GnuWin32/include
-CFLAGS=-ansi -Wall -Os -I tools/include -DBUILD_CPXSTATIC -I ~/.wine/drive_c/Program\ Files/GnuWin32/include
-LDFLAGS=-L ~/.wine/drive_c/Program\ Files/GnuWin32/lib
+CFLAGS=-ansi -Wall -Os -I tools/include -DBUILD_CPXSTATIC -I ~/.wine/drive_c/Program\ Files\ \(x86\)/GnuWin32/include
+LDFLAGS=-L ~/.wine/drive_c/Program\ Files\ \(x86\)/GnuWin32/lib
 
 # compiler and linker
 #CC=cc
-#CC=i486-mingw32-gcc
-CC=i586-mingw32msvc-gcc
+CC=i486-mingw32-gcc
+#CC=i586-mingw32msvc-gcc
