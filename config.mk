17d899ec0ab9fb586234c7b60a9dcfae9f05b35f
Christophe-Marie Duquesne <chm.duquesne@gmail.com>
2012-02-24 13:51:10 +0100

    simplifying rules in makefile

--- config.mk
+++ config.mk
@@ -5,14 +5,14 @@ DESTDIR=/usr/local
 PREFIX=/usr/local
 
 # LINUX
-CFLAGS=-ansi -Wall -Os -I tools/include
-LDFLAGS=
-CC=cc
+#CFLAGS=-ansi -Wall -Os -I tools/include
+#LDFLAGS=
+#CC=cc
 
 # WINE (ubuntu)
-#CFLAGS=-ansi -Wall -Os -I tools/include -DBUILD_CPXSTATIC -I ~/.wine/drive_c/Program\ Files/GnuWin32/include
-#LDFLAGS=-L ~/.wine/drive_c/Program\ Files/GnuWin32/lib
-#CC=i586-mingw32msvc-gcc
+CFLAGS=-ansi -Wall -Os -I tools/include -DBUILD_CPXSTATIC -I ~/.wine/drive_c/Program\ Files/GnuWin32/include
+LDFLAGS=-L ~/.wine/drive_c/Program\ Files/GnuWin32/lib
+CC=i586-mingw32msvc-gcc
 
 # WINE (archlinux)
 #CFLAGS=-ansi -Wall -Os -I tools/include -DBUILD_CPXSTATIC -I ~/.wine/drive_c/Program\ Files\ \(x86\)/GnuWin32/include
