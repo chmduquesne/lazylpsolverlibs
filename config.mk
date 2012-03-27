095f9c58f85c1b8a4e18cb997c732ca40e16bdc2
Christophe-Marie Duquesne <chm.duquesne@gmail.com>
2012-03-27 15:39:37 +0200

    improving the build system

--- config.mk
+++ config.mk
@@ -1,37 +1,38 @@
-VERSION=0.1.1
-DESTDIR=/usr/local
+VERSION = 0.1.2
+DESTDIR = /usr/local
 
 # paths
-PREFIX=/usr/local
+PREFIX = /usr/local
 
-TARGET=LINUX
+TARGET = LINUX
 
 # flags for building a wine version (ubuntu)
 ifeq ($(TARGET),WINE)
-
-CFLAGS=-g -Wall -O2 -I tools/include
-LDFLAGS=
-CC=i586-mingw32msvc-gcc
-LIB=lib/lazycplex.dll lib/lazyxprs.dll lib/lazygurobi.dll lib/lazyglpk.dll
-BIN=bin/test_lazylpsolverlibs.exe
-PKGCONFIG=PKG_CONFIG_LIBDIR=~/.wine/drive_c/Program\ Files/GnuWin32/lib/pkgconfig pkg-config --define-variable=prefix='/home/cduquesne/.wine/drive_c/Program\ Files/GnuWin32/'
-
+  CFLAGS          = -g -Wall -O2 -I tools/include
+  LDFLAGS         =
+  CC              = i586-mingw32msvc-gcc
+  PKGCONFIG       = PKG_CONFIG_LIBDIR=tools/wine/lib/pkgconfig pkg-config --define-variable=prefix=tools/wine
+  LIBPREFIX       =
+  LIBSUFFIX       = .dll
+  BINSUFFIX       = .exe
+  LINKCMD         = $(CC) -shared
+  BUILDLIB_CFLAGS = -DBUILDING_LAZYLPSOLVERLIBS -DBUILD_CPXSTATIC -I include
 endif
-
 # flags for building a native version (ubuntu)
 ifeq ($(TARGET),LINUX)
-
-CFLAGS=-g -Wall -O2 -I tools/include
-LDFLAGS=
-CC=cc
-LIB=lib/liblazycplex.so lib/liblazyxprs.so lib/liblazygurobi.so lib/liblazyglpk.so
-BIN=bin/test_lazylpsolverlibs
-FPM=/var/lib/gems/1.8/gems/fpm-0.3.11/bin/fpm
-PKGCONFIG=pkg-config
-
+  CFLAGS          = -g -Wall -O2 -I tools/include
+  LDFLAGS         =
+  CC              = cc
+  PKGCONFIG       = pkg-config
+  LIBPREFIX       = lib
+  LIBSUFFIX       = .so
+  BINSUFFIX       =
+  LINKCMD         = $(CC) -shared
+  BUILDLIB_CFLAGS = -fPIC -I include
 endif
 
-# Note: (archlinux)
-#CFLAGS=-Wall -Os -I tools/include -I ~/.wine/drive_c/Program\ Files\ \(x86\)/GnuWin32/include
-#LDFLAGS=-L ~/.wine/drive_c/Program\ Files\ \(x86\)/GnuWin32/lib
-#CC=i486-mingw32-gcc
+GMODULE_CFLAGS    = `$(PKGCONFIG) --cflags gmodule-2.0`
+GMODULE_LDFLAGS   = `$(PKGCONFIG) --libs gmodule-2.0`
+LAZYLPSOLVERLIBS_CFLAGS  = -I include
+LAZYLPSOLVERLIBS_LDFLAGS = -llazycplex -llazyxprs -llazygurobi -llazyglpk -Llib
+FPM               = /var/lib/gems/1.8/gems/fpm-0.3.11/bin/fpm
